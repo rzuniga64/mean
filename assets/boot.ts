51ee7655faf8da253689d8d06ae93a6e590df039
Raul Zuniga <rzuniga64@gmail.com>
2016-09-22 20:36:38 -0500

    Import typings into boot.js

--- assets/boot.ts
+++ assets/boot.ts
@@ -1,4 +1,4 @@
-///<reference path="../../typings/index.d.tx" />
+///<reference path="../typings/index.d.ts" />
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './component/app/app.module';
