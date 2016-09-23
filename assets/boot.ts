f32e43d506bde1eac4bbcf0d03448a87e7664e64
Raul Zuniga <rzuniga64@gmail.com>
2016-09-22 19:07:51 -0500

    Add boot.ts for Angular

---
+++ assets/boot.ts
@@ -0,0 +1,6 @@
+///<reference path="../../typings/index.d.tx" />
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './component/app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
