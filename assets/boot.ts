3b1b00bc38d39c05d4b6c8bb6f8d1b6dd0a318a9
Raul Zuniga <rzuniga64@gmail.com>
2016-09-22 21:14:35 -0500

    Rearrange assets folder

--- assets/boot.ts
+++ assets/boot.ts
@@ -1,6 +1,6 @@
 ///<reference path="../typings/index.d.ts" />
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './component/app/app.module';
+import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
