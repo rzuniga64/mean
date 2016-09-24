6662761046bfed64c4479f622daf2efcfa14a605
Raul Zuniga <rzuniga64@gmail.com>
2016-09-24 02:30:59 -0500

    Remove FormGroup, FormBuilder & FormControl

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,5 +1,6 @@
 import {NgModule}                   from '@angular/core';
 import {BrowserModule}              from '@angular/platform-browser';
+import {FormsModule}                from '@angular/forms';
 import {AppComponent}               from './app.component';
 import {MessageComponent}           from "./messages/message.component";
 import {MessageListComponent}       from "./messages/message-list.component";
@@ -10,7 +11,10 @@ import {MessageService}             from "./messages/message.service";
 import {ErrorService}               from "./errors/error.service";
 
 @NgModule({
-    imports: [ BrowserModule ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
     declarations: [
         AppComponent,
         MessageComponent,
