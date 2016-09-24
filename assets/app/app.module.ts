3bb86dfc7fb39ada9889f74dc4a8e880e2b6fc08
Raul Zuniga <rzuniga64@gmail.com>
2016-09-23 21:17:50 -0500

    Add message component

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule }                 from '@angular/core';
 import { BrowserModule }            from '@angular/platform-browser';
 import {AppComponent}               from './app.component';
+import {MessageComponent}           from "./messages/message.component";
 import {HeaderComponent}            from "./header.component";
 
 @NgModule({
   imports: [ BrowserModule ],
   declarations: [
       AppComponent,
+      MessageComponent,
       HeaderComponent
   ],
   bootstrap: [ AppComponent ]
---
+++ assets/app/messages/message.component.ts
@@ -0,0 +1,61 @@
+import {Component, Input, EventEmitter, Output} from "@angular/core";
+import {Message} from "./message";
+import {MessageService} from "./message.service";
+import {ErrorService} from "../errors/error.service";
+
+@Component ({
+    selector: 'my-message',
+    template: ` 
+        <article class="'panel panel-default">
+            <div class="panel-body">
+                {{ message.content }}
+            </div>
+            <footer class="panel-footer">
+            <div class="author">
+                {{ message.username }}
+            </div>
+            <div class="config" *ngIf="belongsToUser()">
+                <a  (click)="onEdit()">Edit</a>
+                <a  (click)="onDelete()">Delete</a>
+            </div>
+            </footer>
+        </article>    
+    `,
+    styles: [`
+        .author {
+            display: inline-block;
+            font-style: italic;
+            font-size: 12px;
+            width: 80%;
+        }
+        .config {
+            display: inline-block;
+            text-align: right;
+            font-size: 12px;
+            width: 19%;
+        }
+    `]
+})
+
+export class MessageComponent {
+    @Input() message: Message;  //message is now bindable from outside
+    @Output() editClicked = new EventEmitter<string>(); //custom event will emit a string
+
+    constructor(private _messageService: MessageService, private _errorService: ErrorService){}
+
+    onEdit() {
+        this._messageService.editMessage(this.message);
+    }
+
+    onDelete() {
+        this._messageService.deleteMessage(this.message)
+            .subscribe(
+                data => console.log(data),
+                error => this._errorService.handleError(error)
+            );
+    }
+
+    belongsToUser() {
+        return localStorage.getItem('userid') == this.message.userId;
+    }
+}
