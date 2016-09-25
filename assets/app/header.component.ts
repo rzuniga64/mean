1b2be15f76546f6da7bebc690e32c68a57290091
Raul Zuniga <rzuniga64@gmail.com>
2016-09-25 00:44:42 -0500

    add routerLinks

--- assets/app/header.component.ts
+++ assets/app/header.component.ts
@@ -5,8 +5,8 @@ import { Component } from "@angular/core";
         <header class="row">
             <nav class="col-md-8 col-md-offset-2">
                 <ul class="nav nav-pills">
-                    <li>Messages</li>
-                    <li>Authentication</li>
+                    <li><a [routerLink]="['']">Messages</a></li>
+                    <li><a [routerLink]="['auth']">Authentication</a></li>
                 </ul>
             </nav>
         </header>
