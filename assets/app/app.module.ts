f790fdf4e815488ebde0bb5797bd4013f43e11fb
Raul Zuniga <rzuniga64@gmail.com>
2016-09-25 00:49:11 -0500

    Add FormBuilder as a provider

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,6 +2,7 @@ import {NgModule}                   from '@angular/core';
 import {BrowserModule}              from '@angular/platform-browser';
 import {RouterModule}               from "@angular/router";
 import {FormsModule}                from '@angular/forms';
+import {FormBuilder}                from '@angular/forms';
 import {AppComponent}               from './app.component';
 import {MessageComponent}           from "./messages/message.component";
 import {MessageListComponent}       from "./messages/message-list.component";
@@ -44,7 +45,8 @@ import {ErrorService}               from "./errors/error.service";
         MessageService,
         AuthService,
         ErrorService,
-        appRoutingProviders
+        appRoutingProviders,
+        FormBuilder
     ]
 })
 export class AppModule { }
