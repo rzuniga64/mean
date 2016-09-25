09cb7c4e72b1794d5a63fbc5a1f7e556c310bea1
Raul Zuniga <rzuniga64@gmail.com>
2016-09-24 20:46:53 -0500

    Wrap the container properly

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>A MongoDB Express Angular NodeJS App</h1>
         <div class="container">
+            <h2>A MongoDB Express Angular NodeJS App</h2>
             <my-header></my-header>
         </div>
     `
