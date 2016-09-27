9e33b1aa6f8ae757a241e42c536ee7121c6fd353
Raul Zuniga <rzuniga64@gmail.com>
2016-09-26 21:18:15 -0500

    Add email validator

---
+++ assets/app/emailValidator.ts
@@ -0,0 +1,16 @@
+import {FormControl} from "@angular/forms";
+
+interface validationResult {
+  [key: string]: boolean;
+}
+
+export class EmailValidator {
+  static invalidEmail(control: FormControl) {
+    if (control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
+      return null;
+    } else {
+      return {'invalidEmail': true}
+    }
+  }
+}
+
