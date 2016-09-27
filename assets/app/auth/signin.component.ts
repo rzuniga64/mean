ea6158a9dc13821293b36f77f62dbd539b263979
Raul Zuniga <rzuniga64@gmail.com>
2016-09-27 01:34:03 -0500

    Update signin.component.ts for release of Angular 2

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from "@angular/core";
-import {FormBuilder, Validators, FormControl, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, FormControl, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
+import {ErrorService} from "../errors/error.service";
 import {User} from "./user";
 import {AuthService} from "./auth.service";
-import {ErrorService} from "../errors/error.service";
+import {EmailValidator} from '../emailValidator';
 
 @Component({
     selector: 'my-signin',
     template: `
         <section class="col-md-8 col-md-offset-2">
-            <form signinForm="ngForm" (ngSubmit)="onSubmit()">
+            <form #signinForm="ngForm" (ngSubmit)="onSubmit(signinForm.value)">
                 <div class="form-group">
                     <label for="email">Mail</label>
                     <input type="email" class="form-control" id="email" name="email" required [ngModel]="email" #email="ngModel" >
@@ -18,36 +19,31 @@ import {ErrorService} from "../errors/error.service";
                     <label for="password">Password</label>
                     <input type="password" class="form-control" id="password" name="password" required [ngModel]="content" #content="ngModel" >
                 </div>
-                <button type="submit" class="btn btn-primary" [disabled]="!signinForm.valid">Sign In</button>
+                <button type="submit" class="btn btn-primary" [disabled]="!signinForm.valid">Sign Up</button>
             </form>
         </section>
     `
 })
+
 export class SigninComponent implements OnInit {
-
     signinForm: FormGroup;
-    email: FormControl;
-    password: FormControl;
 
-    constructor(private _fb:FormBuilder, private _authService: AuthService, private _router: Router, private _errorService: ErrorService) {}
+    constructor(private _fb:FormBuilder, private _authService: AuthService, private _router: Router, private _errorService: ErrorService) { }
 
     ngOnInit() {
 
-        this.email = new FormControl(['', Validators.compose([
-            Validators.required,
-            this.isEmail
-        ])]);
-
-        this.password = new FormControl('', Validators.required);
-        
         this.signinForm = this._fb.group({
-            email: this.email,
-            password: this.password
+            email: new FormControl(['', Validators.compose([
+                Validators.required,
+                EmailValidator.invalidEmail
+            ])]),
+            password: new FormControl('', Validators.required)
         });
     }
 
-    onSubmit() {
-        const user = new User(this.signinForm.value.email, this.signinForm.value.password);
+
+    onSubmit(form) {
+        const user = new User(form.email, form.password);
         this._authService.signin(user)
             .subscribe(
                 data => {
@@ -57,11 +53,5 @@ export class SigninComponent implements OnInit {
                 },
                 error => this._errorService.handleError(error)
             )
-    }
-
-    private isEmail(control: FormControl): {[s: string]: boolean} {
-        if (!control.value.match("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")) {
-            return {invalidMail: true};
-        }
     }
 }
