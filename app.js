154aa6577ce05944b186deea64bc690188d05caf
Raul Zuniga <rzuniga64@gmail.com>
2016-11-06 21:02:52 -0600

    Update app.js to allow Angular 2 to handle router errors.

--- app.js
+++ app.js
@@ -43,29 +43,9 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: {}
-  });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.render('index');
 });
-
 
 module.exports = app;
