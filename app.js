019ec4b0dbac540162fd3c897bb4eb38e3d5980a
Raul Zuniga <rzuniga64@gmail.com>
2016-11-21 02:17:15 -0600

    Update app.js to include correct path for favicon.ico

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-//app.use(favicon(path.join("public", 'images', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, "public", 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
